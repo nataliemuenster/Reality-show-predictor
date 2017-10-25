3ce894b7c60a2ba466da54af6ca5eac96b09963d
Jeffrey Woo <Jeff@DN51qg25.SUNet>
2017-10-25 15:22:20 -0700

    readFiles for political speech...updated baseline.py

--- baseline.py
+++ baseline.py
@@ -5,7 +5,8 @@ import collections
 import math
 import sys
 import numpy as np
-
+import os
+import random
 
 stopList = set(readFile('./english.stop'))
 
@@ -18,10 +19,26 @@ def filterStopWords(self, words):
         filtered.append(word)
     return filtered
 
+def readFiles(directoryName):
+	dataList = []
+	for fileName in os.listdir(directoryName):
+		fileName = os.path.join(directoryName, fileName)
+		dataFile = open(fileName, 'r')
+		dataList.append(({'text': dataFile, 'type': 'speech'}), random.randint(-1, 1))
 
 
 
-trainExamples = util.readExamples('???')
-valExamples = util.readExamples('???')
-testExamples = util.readExamples('???')
+#trainExamples = util.readExamples('???')
+#valExamples = util.readExamples('???')
+#testExamples = util.readExamples('???')
+
+def main(argv):
+    if len(argv) < 2:
+        print >> sys.stderr, 'Usage: python readDate.py <directory name>'
+        sys.exit(1)
+        #'../cs221\ project/congressional\ vote-small/
+    dataDict = readFiles(argv[1])
+
+if __name__ == '__main__':
+    main(sys.argv)
 
