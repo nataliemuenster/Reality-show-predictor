ac019f8ff61ab73861535028ace416eeb356a885
Jeffrey Woo <Jeff@DN0a24a745.SUNet>
2017-12-02 15:56:54 -0800

    implemented majority of unsupervised. Missing hinge loss portion. Created naive bayes for unsupervised, Natalie should check this?

--- naive_bayes.py
+++ naive_bayes.py
@@ -83,3 +83,79 @@ class NaiveBayes:
 
     self.nDocs += 1
 
+class NaiveBayesUnsupervised:
+  def __init__(self):
+    # maps class to list of [word count, doc count]
+    #self.classCounts = collections.defaultdict(lambda: [0, 0])
+    # maps word --> class --> instances of word in class
+    #self.wordCounts = collections.defaultdict(lambda: collections.defaultdict(lambda: 0))
+    
+    self.wordCountsForClass = collections.defaultdict(lambda:collections.defaultdict(lambda: [0.0,0.0]))
+    self.docCount = collections.defaultdict(lambda: 0.0)
+    self.wordCounts = collections.defaultdict(lambda:[0.0,0.0])
+
+    # total number of docs
+    self.nDocs = 0.0
+    self.vocab = set() #build vocabulary of unique words for pos and neg classes
+    self.stopWords = self.readStopWordsFile('./english.stop')
+
+  def readStopWordsFile(self, fileName):
+    contents = []
+    f = open(fileName)
+    for line in f:
+      contents.append(line)
+    f.close()
+    return ('\n'.join(contents)).split()
+
+  def filterStopWords(self, words):
+    """Filters stop words."""
+    filtered = []
+    for word in words:
+      if not word in self.stopWords and word.strip() != '':
+        filtered.append(word)
+    return filtered
+
+
+  def classify(self, wordCount):
+    uniqueWords = set()
+    for word in wordCount:
+      uniqueWords.add(word)
+    
+    klass = -1
+    leftCalc = math.log(self.docCount[-1] + 1)
+    leftCalc -= math.log(self.docCount[1] + self.docCount[-1])
+    rightCalc = math.log(self.docCount[1] + 1)
+    rightCalc -= math.log(self.docCount[1] + self.docCount[-1])
+
+    leftDenom = self.wordCounts[-1][0] + len(self.vocab) #3rd index 1 or 0??
+    rightDenom = self.wordCounts[1][0] + len(self.vocab)
+
+    for word in uniqueWords:
+        rightCalc += math.log(self.wordCountsForClass[1][word][0] + 1)
+        rightCalc -= math.log(rightDenom)
+        leftCalc += math.log(self.wordCountsForClass[-1][word][0] + 1)
+        leftCalc -= math.log(leftDenom)
+
+    if rightCalc > leftCalc:
+      klass = 1
+
+    return klass
+
+
+  #takes in text in doc and the doc's class
+  def train(self, klass, wordCount):
+    # number of appearances of word in docs with classification klass
+    uniqueWords = set()
+    for word in wordCount:
+      self.vocab.add(word) #build vocabulary of unique words for pos and neg classes
+      uniqueWords.add(word)
+      self.wordCounts[klass][0] += wordCount[word]
+      self.wordCountsForClass[klass][word][0] += wordCount[word]
+
+    self.docCount[klass] += 1
+    self.wordCounts[klass][1] += len(uniqueWords)
+    for uniq in uniqueWords:
+      self.wordCountsForClass[klass][uniq][1] += 1
+
+    self.nDocs += 1
+
--- unsupervised.py
+++ unsupervised.py
@@ -11,65 +11,126 @@ import time
 import kmeans
 from sklearn.metrics import precision_recall_fscore_support
 
+stopWords = readStopWordsFile('./english.stop')
+
+def readStopWordsFile(fileName):
+    contents = []
+    f = open(fileName)
+    for line in f:
+      contents.append(line)
+    f.close()
+    return ('\n'.join(contents)).split()
+
+def filterStopWords(words):
+    """Filters stop words."""
+    filtered = []
+    for word in words:
+      if not word in stopWords and word.strip() != '':
+        filtered.append(word)
+    return filtered
+
+def createWordCountDict(text):
+    wordDict = {}
+    words = text.translate(None, string.punctuation).lower().split()
+    words = filterStopWords(words)
+    for word in words:
+        if word in wordDict:
+            wordDict[word] += 1
+        else:
+            wordDict[word] = 1
+    return wordDict
+
+def findBestSplit(hingeDict, sourceList, startIndex, dataBySource, dataList, liberalSplit, conservativeSplit, wordCountList):
+    if startIndex == len(sourceList):
+
+        # calculate hingeLoss by testing on numDev
+        # update hingeDict
+    else:
+        newLiberal = list(liberalSplit)
+        newLiberal.append(sourceList[startIndex])
+        newConservative = list(conservativeSplit)
+        findBestSplit(hingeDict, sourceList, startIndex + 1, dataBySource, dataList, newLiberal, newConservative, wordCountList)
+
+        newLiberal = list(liberalSplit)
+        newConservative = list(conservativeSplit)
+        newConservative.append(sourceList[startIndex])
+        findBestSplit(hingeDict, sourceList, startIndex + 1, dataBySource, dataList, newLiberal, newConservative, wordCountList)
+
+
 #python unsupervised.py ../cs221-data/read-data/ ./labeled_data.txt 
+# Currently Naive Bayes specific, could be generalized
 def main(argv):
     if len(argv) < 3:
-        print >> sys.stderr, 'Usage: python unsupervised_kmeans.py <data directory name> <labels file name>'
+        print >> sys.stderr, 'Usage: python unsupervised.py <data directory name> <labels file name>'
         sys.exit(1)
     classificationDict = util.createClassDict(argv[2])
     dataList = util.readFiles(argv[1], classificationDict) #if no classificationDict passed in, randomized
-    labeledData, unlabeledData = util.separateLabeledExamples(dataList) 
-    
+
+    # index in list = exampleNum
+    # value at index = wordcountDict
+    wordCountList = []
+    for value in dataList:
+        wordCountList.append(createWordCountDict(value[1]['text']))
+
+    labeledData, unlabeledData = util.separateLabeledExamples(dataList)
+
+
     random.shuffle(labeledData)
-    numDev = len(labeledData) / 2 #training set = 50% of the labeled data
-    numTest = len(labeledData) / 2 #training set = 50% of the labeled data
-    numDevCorrect = 0
+
+    # Key = Source
+    # Value = List of data points
+    sourceList = []
+    dataBySource = {}
+    for dataPoint in dataList:
+        if dataPoint['publication'] in dataBySource:
+            dataBySource[dataPoint['publication']].append(dataPoint)
+        else:
+            sourceList.append(dataPoint['publication'])
+            dataBySource[dataPoint['publication']] = [dataPoint]
+    # Key = hinge loss. The smallest key will be the one with the lowest global loss and best assignment
+    # Value = Tuple ([], [], NaiveBayes()) where the first element is the liberal sources, and the second element is the conservative sources
+    hingeDict = {}
+
+    # "Dev" Phase
+    findBestSplit(hingeDict, sourceList, 0, dataBySource, dataList, [], [], wordCountList)
+
+    # Find best result from "Dev" Phase
+    minLoss = float('inf')
+    splitTuple = None
+    for lossKey in hingeDict:
+        if lossKey < minLoss:
+            minLoss = lossKey
+            splitTuple = hingeDict[lossKey]
+
+    # Test Phase
     numTestCorrect = 0
-    
-    classifier = kmeans.Kmeans(2, 5) #numClusters, maxIterations
-    devSet = labeledData[:numDev]
-    testSet = labeledData[numDev:]
-    
-    #classifier.createExampleVector((0,{"text": "Hello my friend shipwreck"},None))
-    #classifier.createExampleVector((1,{"text": "Where did my planet go moon?"},None))
-    #classifier.createExampleVector((2,{"text": "Moo"},None))
-    for ex in dataList:
-        classifier.createExampleVector(ex)
-    
-    centroids, clusterAssignments, reconstrLoss = classifier.runKmeans()
-
-    devResults = ([],[]) #Y, prediction
-    testResults = ([],[]) #Y, prediction
-
-    print "centroids start at (0,1)"
-    for ex in devSet: #determine which cluster corresponds with which classification by randomly assigning and choosing the best
-        classification = -1 if clusterAssignments[ex[0]] == centroids[0] else 1
-        #print classification
-        if classification == ex[2]:
-            numDevCorrect += 1
-        devResults[0].append(ex[2])
-        devResults[1].append(classification)
-    print "DEV SET NUM CORRECT: " + str(float(numDevCorrect) / numDev)
-    if numDevCorrect > (numDev / 2) else (1,0) #determine whether to switch assignments
-    	centroidAssignment = (0,1) 
-    	devResults[1] = [1-i for i in devResults]
-
-    precision,recall,fscore,support = precision_recall_fscore_support(devResults[0], devResults[1], average='binary')
-	print "Kmeans DEV scores:\n\tPrecision:%f\n\tRecall:%f\n\tF1:%f" % (precision, recall, fscore)
-    print "centroids assigned: " + str(centroidAssignment)
-    #evaluate
-    for ex in testSet:
-        classification = -1 if clusterAssignments[ex[0]] == centroidAssignment[0] else 1
-        print classification
-        if classification == ex[2]:
+    total = len(labeledData)
+    for i in range(labeledData):
+        dataPoint = labeledData[i]
+        klass = dataPoint['klass']
+        guess = None
+        if dataPoint['publication'] in splitTuple[0]:
+            guess = -1
+        else:
+            guess = 1
+        if klass == guess:
             numTestCorrect += 1
-        testResults[0].append(ex[2])
-        testResults[1].append(classification)
+    print "numCorrect: " + str(numTestCorrect) + ' numTotal: ' + str(total) + ' percentage: ' + str(float(numTestCorrect) / total)
 
-    print "numCorrect: " + str(numTestCorrect) + ' numTotal: ' + str(numTest) + ' percentage: ' + str(float(numTestCorrect) / numTest)
-    precision,recall,fscore,support = precision_recall_fscore_support(testResults[0], testResults[1], average='binary')
-    print "Kmeans TEST scores:\n\tPrecision:%f\n\tRecall:%f\n\tF1:%f" % (precision, recall, fscore)
 
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 if __name__ == '__main__':
     for _ in xrange(10):
